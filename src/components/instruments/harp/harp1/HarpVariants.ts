f316e43daff917a8772c44a76108014118ed1ae8
Kamlesh Gupta <onlinevirtualinstrument@gmail.com>
2025-05-14 15:25:41 +0530

    changed

--- src/components/instruments/harp/harp1/HarpVariants.ts
+++
@@ -1,8 +0,0 @@
-
-export const harpVariants = [
-  { id: 'standard', name: 'Standard Harp' },
-  { id: 'celtic', name: 'Celtic Harp' },
-  { id: 'concert', name: 'Concert Harp' },
-  { id: 'classical', name: 'Classical Harp' },
-  { id: 'electric', name: 'Electric Harp' }
-];
--- src/utils/blog/firebase-config.ts
+++
@@ -1,6 +0,0 @@
-
-import { getFirestore } from 'firebase/firestore';
-import { app } from '@/utils/auth/firebase';
-
-// Initialize Cloud Firestore
-export const db = getFirestore(app);
