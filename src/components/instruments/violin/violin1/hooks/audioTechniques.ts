1ec43e1ee98d0430dd7d6ff7274d0232693ce5e7
Kamlesh Gupta <onlinevirtualinstrument@gmail.com>
2025-06-16 19:36:42 +0530

    locally changed

--- src/components/instruments/violin/violin1/hooks/audioTechniques.ts
+++
@@ -1,6 +0,0 @@
-
-// Use the optimized techniques from the optimized file
-export { applyTechnique, applyReverb } from './optimizedAudioTechniques';
-
-// Export advanced sound shaping techniques
-export { addStringResonance, createBowedString } from './advancedTechniques';
